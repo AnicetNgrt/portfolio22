5c0471809970c17e9163b82249a99f9b2c0cf55a
Anicet Nougaret <anicet.nougaret@zohomail.eu>
2022-11-05 12:00:09 +0100

    final ajdustments

--- src/comps/roles.svelte
+++ src/comps/roles.svelte
@@ -25,20 +25,20 @@
     $: inverseDistance = 1 - distance
     $: computedSpeed = (distance-0.5)*0.5
 
-    let roles = [
+    const ROLES = [
         "and an indie game creator",
         "currently living in Lyon",
         "with experience in Web3",
-        "while exploring multiple paradigms",
-        "and is a full-stack engineer",
+        "always exploring multiple paradigms",
+        "and I am a full-stack engineer",
         "who also does UX/UI design",
         "whom can work in Paris too",
         "currently 21 years old if I don't forget to update this website",
         "sometimes a private programming tutor",
-        "an INSA Lyon Computer Science 4th year student",
-        "can speak French and English very fluently",
+        "and an INSA Lyon Computer Science 4th year student",
+        "whom speaks French and English fluently",
         "knowing some Spanish and Russian as well",
-        "significant programming language polyglot",
+        "as a significant programming language polyglot",
         "hobbyist illustrator as well",
         "and a gamejamer",
         "also a pixelartist",
@@ -46,14 +46,14 @@
         "understanding some Rust",
         "called a front-end ninja by some",
         "co-founding a tiny DeFi startup",
-        "extensively used Vim in the past, honestly use paper instead",
-        "winning hackathons",
-        "proficient with software architecture",
+        "and I extensively used Vim, honestly use paper instead",
+        "while winning hackathons",
+        "and proficient with software architecture",
         "totally able to do complex backend tasks as well",
         "and absolutely doing too many things at once",
-        "enjoying the creative web",
-        "as you guessed, who likes spinny things"
+        "and enjoying the creative web as you may have guessed",
     ]
+    let roles = [...ROLES]
     shuffle(roles)
 
     function wave(x: number) {
@@ -65,8 +65,8 @@
     <div class="marquee-container">
         <Marquee {stoppedPerformance} {onPerfRecorded} externalStop={stop} speed={computedSpeed} speedMultiplier={speed}>
             <div class="marquee-content">
-                {#each [...roles] as role, i}
-                    <div class="role" style={`opacity: ${wave(distance)+0.2}; filter: blur(${(1-(wave(distance)))*10}px) hue-rotate(${(inverseDistance*Math.random())*380}deg); line-height: ${(0.4+inverseDistance*size)*1.5}rem; font-size: ${0.4+inverseDistance*size}rem`}>
+                {#each roles as role, i}
+                    <div class="role" style={`opacity: ${wave(distance)+0.2}; filter: blur(${(1-(wave(distance)))*10}px) hue-rotate(${(inverseDistance*(ROLES.indexOf(role)/ROLES.length))*380}deg); line-height: ${(0.4+inverseDistance*size)*1.5}rem; font-size: ${0.4+inverseDistance*size}rem`}>
                         {role}
                     </div>
                 {/each}
